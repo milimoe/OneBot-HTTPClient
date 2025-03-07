d99b84780721a8d33d0517de1dc8460bb08d2406
milimoe <mili@wrss.org>
2025-03-07 19:10:16 +0800

    添加撤回事件监听

---
+++ src/Framework/JsonConverter/GroupRecallEventConverter.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+using Milimoe.FunGame.Core.Library.Common.Architecture;
+using Milimoe.OneBot.Model.Event;
+
+namespace Milimoe.OneBot.Framework.JsonConverter
+{
+    public class GroupRecallEventConverter : BaseEntityConverter<GroupRecallEvent>
+    {
+        public override GroupRecallEvent NewInstance()
+        {
+            return new();
+        }
+
+        public override void ReadPropertyName(ref Utf8JsonReader reader, string propertyName, JsonSerializerOptions options, ref GroupRecallEvent result, Dictionary<string, object> convertingContext)
+        {
+            switch (propertyName)
+            {
+                case "time":
+                    result.time = reader.GetInt64();
+                    break;
+                case "self_id":
+                    result.self_id = reader.GetInt64();
+                    break;
+                case "post_type":
+                    result.post_type = reader.GetString() ?? "";
+                    break;
+                case "notice_type":
+                    result.notice_type = reader.GetString() ?? "";
+                    result.post_sub_type = result.notice_type;
+                    break;
+                case "group_id":
+                    result.group_id = reader.GetInt64();
+                    break;
+                case "user_id":
+                    result.user_id = reader.GetInt64();
+                    break;
+                case "operator_id":
+                    result.operator_id = reader.GetInt64();
+                    break;
+                case "message_id":
+                    result.message_id = reader.GetInt64();
+                    break;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, GroupRecallEvent value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber("time", value.time);
+            writer.WriteNumber("self_id", value.self_id);
+            writer.WriteString("post_type", value.post_type);
+            writer.WriteString("notice_type", value.notice_type);
+            writer.WriteNumber("group_id", value.group_id);
+            writer.WriteNumber("user_id", value.user_id);
+            writer.WriteNumber("operator_id", value.operator_id);
+            writer.WriteNumber("message_id", value.message_id);
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ src/Model/Event/GroupRecallEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Milimoe.OneBot.Framework.Base;
+
+namespace Milimoe.OneBot.Model.Event
+{
+    [method: JsonConstructor]
+    public class GroupRecallEvent(long time = 0, long self_id = 0, string post_type = "", string notice_type = "", long group_id = 0, long user_id = 0, long operator_id = 0, long message_id = 0) : BaseGroupEvent(group_id, post_type)
+    {
+        public long time { get; set; } = time;
+        public long self_id { get; set; } = self_id;
+        public string notice_type { get; set; } = notice_type;
+        public long user_id { get; set; } = user_id;
+        public long operator_id { get; set; } = operator_id;
+        public long message_id { get; set; } = message_id;
+
+        [JsonIgnore]
+        public string detail => $"{user_id} 在群聊 {group_id} 中的消息（ID: {message_id}）被 {operator_id} 撤回";
+    }
+}
