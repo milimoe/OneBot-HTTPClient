ae7896f5f8481ea68d6016c9534849592a46e6ad
milimoe <mili@wrss.org>
2025-08-02 18:19:37 +0800

    添加设置精华

--- src/Model/Content/DeleteMsgContent.cs
+++ src/Model/Content/DeleteMsgContent.cs
@@ -3,9 +3,9 @@ using Milimoe.OneBot.Framework.Interface;
 
 namespace Milimoe.OneBot.Model.Content
 {
-    public class DeleteMsgContent(int message_id) : IContent
+    public class DeleteMsgContent(long message_id) : IContent
     {
-        public int message_id { get; set; } = message_id;
+        public long message_id { get; set; } = message_id;
 
         [JsonIgnore]
         public string detail => "撤回消息：" + message_id;
--- src/Model/Content/EssenceMsgContent.cs
+++ src/Model/Content/EssenceMsgContent.cs
@@ -3,9 +3,9 @@ using Milimoe.OneBot.Framework.Interface;
 
 namespace Milimoe.OneBot.Model.Content
 {
-    public class EssenceMsgContent(int message_id) : IContent
+    public class EssenceMsgContent(long message_id) : IContent
     {
-        public int message_id { get; set; } = message_id;
+        public long message_id { get; set; } = message_id;
 
         [JsonIgnore]
         public string detail => "设置精华：" + message_id;
